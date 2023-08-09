40f8ae9f4760874acf65ef46c5c764c16b3f5601
zhuwbigdata <zhuw.bigdata@gmail.com>
2023-08-09 09:11:51 -0500

    Update evaluatenotebookruns.py

--- cicd_scripts/evaluatenotebookruns.py
+++ cicd_scripts/evaluatenotebookruns.py
@@ -1,8 +1,4 @@
 # evaluatenotebookruns.py
-#!/usr/bin/python3
-import io
-import xmlrunner
-from xmlrunner.extra.xunit_plugin import transform
 import unittest
 import json
 import glob
@@ -10,59 +6,39 @@ import os
 
 class TestJobOutput(unittest.TestCase):
 
-  test_output_path = '/home/vsts/work/r1/a/_CICD-Demo/Databricks/logs/json'
-  unravel_output_path = '/home/vsts/work/r1/a/_CICD-Demo/Databricks/logs/unravel'
+  test_output_path = '#ENV#'
 
   def test_performance(self):
-    path = self.test_output_path
-    statuses = []
+      path = self.test_output_path
+      statuses = []
 
-    for filename in glob.glob(os.path.join(path, '*.json')):
-      print('Evaluating: ' + filename)
-      data = json.load(open(filename))
-      duration = data['execution_duration']
-      if duration > 100000:
-        status = 'FAILED'
-      else:
-        status = 'SUCCESS'
+      for filename in glob.glob(os.path.join(path, '*.json')):
+          print('Evaluating: ' + filename)
+          data = json.load(open(filename))
+          duration = data['execution_duration']
+          if duration > 100000:
+              status = 'FAILED'
+          else:
+              status = 'SUCCESS'
 
-      statuses.append(status)
+          statuses.append(status)
 
-    self.assertFalse('FAILED' in statuses)
+      self.assertFalse('FAILED' in statuses)
 
   def test_job_run(self):
-    path = self.test_output_path
-    statuses = []
+      path = self.test_output_path
+      statuses = []
 
-    for filename in glob.glob(os.path.join(path, '*.json')):
-      print('Evaluating: ' + filename)
-      data = json.load(open(filename))
-      status = data['state']['result_state']
-      statuses.append(status)
+      for filename in glob.glob(os.path.join(path, '*.json')):
+          print('Evaluating: ' + filename)
+          data = json.load(open(filename))
+          status = data['state']['result_state']
+          statuses.append(status)
 
-    self.assertFalse('FAILED' in statuses)
-
-  def test_unravel_check(self):
-    path = self.unravel_output_path
-    statuses = []
-
-    filename = path + '/unravel.json'
-    unrvel_md = None
-    data = json.load(open(filename))
-    if (data['insights'] == True) or (data['recommendations'] == True):
-      msgfile =  path + '/unravel.md'
-      with open(msgfile,'r') as file:
-        unravel_md = file.read()
-      status = 'FAILED'
-    else:
-      status = 'SUCCESS'
-    statuses.append(status)
-
-    self.assertFalse('FAILED' in statuses, msg = unravel_md)
-
+      self.assertFalse('FAILED' in statuses)
 
 if __name__ == '__main__':
-  out = io.BytesIO()
+  unittest.main()
 
   unittest.main(testRunner=xmlrunner.XMLTestRunner(output=out),
     failfast=False, buffer=False, catchbreak=False, exit=False)
