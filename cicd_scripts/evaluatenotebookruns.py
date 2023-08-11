bf65c838601611d9225314cb79b89d471b0f3cac
zhuwbigdata <zhuw.bigdata@gmail.com>
2023-08-11 11:39:45 -0500

    Update evaluatenotebookruns.py

--- cicd_scripts/evaluatenotebookruns.py
+++ cicd_scripts/evaluatenotebookruns.py
@@ -7,6 +7,7 @@ import os
 class TestJobOutput(unittest.TestCase):
 
   test_output_path = '#ENV#'
+  unravel_output_path = "#UNRAV#'
 
   def test_performance(self):
       path = self.test_output_path
@@ -37,6 +38,24 @@ class TestJobOutput(unittest.TestCase):
 
       self.assertFalse('FAILED' in statuses)
 
+  def test_unravel_check(self):
+    path = self.unravel_output_path
+    statuses = []
+
+    filename = path + '/unravel.json'
+    unrvel_md = None
+    data = json.load(open(filename))
+    if (data['insights'] == True) or (data['recommendations'] == True):
+      msgfile =  path + '/unravel.md'
+      with open(msgfile,'r') as file:
+        unravel_md = file.read()
+      status = 'FAILED'
+    else:
+      status = 'SUCCESS'
+    statuses.append(status)
+
+    self.assertFalse('FAILED' in statuses, msg = unravel_md)
+    
 if __name__ == '__main__':
   unittest.main()
 
